196331f00f73f6547cc1c712e5783aa97be754bf
OussamaSALAHOUELHADJ <samaoussama24@gmail.com>
2020-04-05 06:18:41 +0100

    fixing errors

--- app/src/main/java/com/example/crisis/homevolunteer.java
+++ app/src/main/java/com/example/crisis/homevolunteer.java
@@ -1,14 +1,22 @@
 package com.example.crisis;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class homevolunteer extends AppCompatActivity {
+    public static final String  USERNAME = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homevolunteer);
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(USERNAME);
+        TextView volunteer_name = (TextView) findViewById(R.id.volunteer_name);
+        volunteer_name.setText(username);
+
+
     }
 }
--- app/src/main/java/com/example/crisis/login.java
+++ app/src/main/java/com/example/crisis/login.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+
+    private static final String USERNAME = "username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,10 @@ public class login extends AppCompatActivity {
 
     }
     public void login(View v){
+        TextView user = (TextView) findViewById(R.id.username);
+        String username = user.getText().toString();
         Intent intent = new Intent(this, homevolunteer.class);
+        intent.putExtra(homevolunteer.USERNAME, username);
         startActivity(intent);
-
     }
 }
